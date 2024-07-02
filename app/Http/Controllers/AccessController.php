d60157412e802b0b631d2506ed7e73317ece9044
Sanjida Islam <islamsanjida1621@gmail.com>
2024-07-02 16:15:28 +0600

    role assign

--- app/Http/Controllers/AccessController.php
+++ app/Http/Controllers/AccessController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 
 
 class AccessController extends Controller
 {
+    public function get_roles()
+    {
+        $role = Role::all();
+        return $role;
+    }
+
     public function hasRole(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -20,6 +28,11 @@ class AccessController extends Controller
             return $validator->errors();
  
         $user = User::find($request->user);
-        return $user->hasRole($request->roles);
+        // return $user->hasRole($request->roles);
+        if ($user->hasRole($request->roles)) {
+            return response()->json(['message' => 'User has the admin role.']);
+        }else{
+            return response()->json(['message' => 'User has not the admin role.']);
+        }
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\User;
+use Spatie\Permission\Models\Role; 
+
 
 class UserSeeder extends Seeder
 {
@@ -15,15 +18,21 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+        $user = User::create([
             'username'  => 'admin',
             'firstname' => 'Admin',
-            'lastname'  => '',
             'email'     => 'admin@gmail.com',
             'password'  => bcrypt(12345678),
             'user_type' => 'admin',
-            'phone'     => '',
         ]);
 
+        $role = Role::where('name','admin')->first();
+
+        DB::table('model_has_roles')->insert([
+            'role_id' => $role->id,
+            'model_type' => 'App\Models\User',
+            'model_id' => $user->id,
+            'team_id' => 1,
+        ]);
     }
 }
